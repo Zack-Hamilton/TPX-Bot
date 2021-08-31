a0d366f86b2bd13d55ed074c5c97d0c25bb5317c
Zack-Hamilton <ahmadhaaroon30@gmail.com>
2021-08-31 11:43:38 +0500

    Cleaned up a bit of the menu and customize commands, and began working on bot reponses.

--- BotTesting.py
+++ BotTesting.py
@@ -64,15 +64,20 @@ def MainMenu(message,admin):
     MessageArr = [""] * Count;
     eof = Read.tell();
     Read.seek(0);
-    Count = 0
+    Count = 0;
+    Array = False;
     while (Read.tell() != eof):
         TempRecord = pickle.load(Read);
         if (admin == False) and (TempRecord.Available == True):
-            MessageArr[Count] = "- " + TempRecord.Content;
+            MessageArr[Count] = "⦿ " + TempRecord.Content;
             Count = Count + 1;
+            Array = True;
         if (admin == True) and (TempRecord.Available == False):
-            MessageArr[Count] = "- " + TempRecord.Content;
+            MessageArr[Count] = "⦿ " + TempRecord.Content;
             Count = Count + 1;
+            Array = True;
+    if not Array:
+        MessageArr = "";
     Read.close();
 
 #Reset Menu
@@ -136,17 +141,17 @@ async def on_message(message):
                 data = requests.get(url).content
                 await TPX.user.edit(avatar=data)
                 Loop = False;
-                return (f"Succesfully changed {TPX.user.name}'s avatar!")
+                return (f"**```Succesfully changed {TPX.user.name}'s avatar!```**")
             #Error
             except Exception as e:
-                return ("Invalid URL or image sent. Please upload the correct URL or image.");
+                return ("**```Invalid URL or image sent. Please upload the correct URL or image.```**");
                 print("Error report:\n\n",repr(e));
                 print("\nTraceback report:\n\n",traceback.format_exc(), end="");
 
         #Nickname Edit
         async def Nickname(nickname):
             await message.guild.me.edit(nick=nickname);
-            await message.channel.send("Successfully updated nickname.")
+            await message.channel.send("**```Successfully updated nickname.```**")
 
         #Status Edit
         async def StatusEdit(status,presence):
@@ -171,72 +176,94 @@ async def on_message(message):
                 Write.close();
             except:
                  if message.author == TPX.user:
-                    await message.channel.send("An unfortunate error has occurred. If this error is reoccurring, please send details of it to my owner so that he may look into it. I apologize for the inconvenience.");
+                    await message.channel.send("```An unfortunate error has occurred. If this error is reoccurring, please send details of it to my owner so that he may look into it. I apologize for the inconvenience.```");
                  print("Error report:\n\n",repr(e));
                  print("\nTraceback report:\n\n",traceback.format_exc(), end="");
 
 #----------Regular Commands
+        #Info
+        if (message.content == "t.info"):
+            await message.channel.send(f":star:Hi there! I'm your local {message.guild.me.display_name}, here to help out with whatever you need!\nJust use t.menu to open up my commands list and see what I can do! Have fun!:grin:");
+        elif(message.content.startswith("t.ask")):
+            if message.content[5:] != "":
+                if "flare" in message.content:
+                    print();
+                else:
+                    await message.channel.send("You asked..." + message.content[5:] + "? I dunno, I don't have a good reponse list yet :upside_down:");                
+            else:
+                await message.channel.send(":eyes:What would you like to ask?")
         #Print Menu
-        if (message.content == "t.menu"):
+        elif (message.content == "t.menu"):
             MainMenu(message,False);
-            await message.channel.send("*Here's a list of available functions:*");
             Loop = True;
             Count = 0;
+            String = "```";
             while Loop:
                 try:
-                    await message.channel.send(MessageArr[Count]);
+                    String = String + MessageArr[Count] + "\n";
                     Count = Count + 1;
                 except Exception as e:
                     Loop = False;
-
+            if Count == 0:
+                await message.channel.send("**```No commands set.```**");
+            else:
+                String = String + "```";
+                await message.channel.send("**```Here's a list of available functions:```**");
+                await message.channel.send(String);
 #----------Admin Commands
         elif message.author.guild_permissions.administrator or Role:
             #----------Menu Commands
             #Add Menu Item
             if (message.content == "t.menuedit"):
-                await message.channel.send("Enter Command Name:");
+                await message.channel.send("**```Enter Command Name:```**");
                 channel = message.channel;
                 def check(m):
                     return m.content != ''  and m.channel == channel;
                 ComName = (await TPX.wait_for('message',check=lambda m: m.author == message.author and m.channel == message.channel)).content;
             
-                await message.channel.send("Admin Only?(Y/N):");
+                await message.channel.send("**```Admin Only?(Y/N):```**");
                 Check = False;
                 while not Check:
                     ComAvail = await TPX.wait_for('message',check=lambda m: m.author == message.author and m.channel == message.channel);
                     if (ComAvail.content == "Y") or (ComAvail.content == "N"):
                         Check = True;
                     else:
-                        await message.channel.send("Invalid Response.");
+                        await message.channel.send("**```Invalid Response.```**");
                 if (ComAvail.content == "N"):
                     Available = True;
                 else:
                     Available = False;
                 AddRec(ComName,Available);
-                await message.channel.send("Successfully Added.");
+                await message.channel.send("**```Successfully Added.```**");
             #Print Admin Menu
             elif (message.content == "t.menuadmin"):
                 MainMenu(message,True);
-                await message.channel.send("*Here's a list of available functions:*");
                 Loop = True;
                 Count = 0;
+                String = "```";
                 while Loop:
                     try:
-                        await message.channel.send(MessageArr[Count]);
+                        String = String + MessageArr[Count] + "\n";
                         Count = Count + 1;
                     except Exception as e:
                         Loop = False;
+                if Count == 0:
+                    await message.channel.send("**```No commands set.```**");
+                else:
+                    String = String + "```";
+                    await message.channel.send("**```Here's a list of available functions:```**");
+                    await message.channel.send(String);
             #Reset Menu
             elif (message.content == "t.menureset"):
                 MenuReset();
-                await message.channel.send("Menu successfully reset.");
-            #Edit Pfp
-            if (message.content.startswith("t.customize")):
+                await message.channel.send("**```Menu successfully reset.```**");
+            #Customize
+            elif (message.content.startswith("t.customize")):
                 #Selection
-                await message.channel.send('''What would you like to change?:
-- Profile Picture(P)
+                await message.channel.send('''**```What would you like to change?:```**
+```- Profile Picture(P)
 - Nickname(N)
-- Status(S)''');
+- Status(S)```''');
                 def check(m):
                     return m.content != ''  and m.channel == channel;
                 Check = False;
@@ -245,11 +272,11 @@ async def on_message(message):
                     if (choice == "P") or (choice == "N") or (choice == "S"):
                         Check = True;
                     else:
-                        await message.channel.send("Invalid Response.");
+                        await message.channel.send("```Invalid Response.```");
 
                 #Profile Edit
                 if (choice == "P"):
-                    await message.channel.send("Enter URL or image:");
+                    await message.channel.send("**```Enter URL or image:```**");
                     Loop = True;
                     while Loop:
                         message = await TPX.wait_for('message',check=lambda m: m.author == message.author and m.channel == message.channel);
@@ -260,33 +287,38 @@ async def on_message(message):
 
                 #Nickname Edit
                 elif (choice == "N"):
-                    await message.channel.send("Enter Nickname:");
+                    await message.channel.send("**```Enter Nickname:```**");
                     Nick = (await TPX.wait_for('message',check=lambda m: m.author == message.author and m.channel == message.channel)).content;
                     await Nickname(Nick);
 
                 #Status Edit
                 elif (choice == "S"):
-                    await message.channel.send("Enter Status Name:");
+                    await message.channel.send("**```Enter Status Name:```**");
                     status = (await TPX.wait_for('message',check=lambda m: m.author == message.author and m.channel == message.channel)).content;
-                    await message.channel.send('''Enter Status Type:
-- Online(O)
+                    await message.channel.send('''**```Enter Status Type:```**
+```- Online(O)
 - Idle(I)
 - Do Not Disturb(D)
 - Invisible(N)
-- Offline(F)''');
+- Offline(F)```''');
                     Check = False;
                     while not Check:
                         presence = (await TPX.wait_for('message',check=lambda m: m.author == message.author and m.channel == message.channel)).content;
                         if (presence == "O") or (presence == "I") or (presence == "D") or (presence == "N") or (presence == "F"):
                             Check = True;
                         else:
-                            await message.channel.send("Invalid Response.");
+                            await message.channel.send("**```Invalid Response.```**");
                     Response = await StatusEdit(status,presence);
-                    await message.channel.send("Status successfully updated.");
-
+                    await message.channel.send("**```Status successfully updated.```**");
+            #Otherwise
+            elif (message.content.startswith("t.")):
+                await message.channel.send(":confounded:I'm sorry, I'm afraid I do not understand that command. Use t.menu for a list of my available commands.");
+#----------Otherwise
+        elif (message.content.startswith("t.")):
+            await message.channel.send(":confounded:I'm sorry, I'm afraid I do not understand that command. Use t.menu for a list of my available commands.");
 #Error Messages
     except Exception as e:
-        await message.channel.send("An unfortunate error has occurred. If this error is reoccurring, please send details of it to my owner so that he may look into it. I apologize for the inconvenience.");
+        await message.channel.send("```An unfortunate error has occurred. If this error is reoccurring, please send details of it to my owner so that he may look into it. I apologize for the inconvenience.```");
         print("Error report:\n\n",repr(e));
         print("\nTraceback report:\n\n",traceback.format_exc(), end="");
 
